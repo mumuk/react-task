d75df29b74e622f909592f519d4bbde995b00460
Mykhailo Lytvynov <patrickarick@gmail.com>
2021-08-23 17:10:06 +0300

    added debounce & onKeyUp event

--- src/components/FiltersBar.js
+++ src/components/FiltersBar.js
@@ -2,14 +2,13 @@ import React, {useState} from 'react';
 import Select from 'react-select'
 import {useDispatch, useSelector} from "react-redux"
 import {setTagsFilter, sortByComments, sortByLikes} from "../actions/filtersActions";
-import {DebounceInput} from "react-debounce-input"
+import debounce from 'lodash.debounce';
 
 export const FiltersBar = () => {
   const dispatch = useDispatch()
   const {isFetching, items} = useSelector(state => state.repos)
   const {text, sortBy} = useSelector(state => state.filters)
   const [inputText, setInputText] = useState(text)
-  const [arrItems, setArrItems] = useState(items)
 
 
   const options = [
@@ -18,11 +17,14 @@ export const FiltersBar = () => {
   ];
 
 
-  const onTextChange = (e) => {
-    console.log('from keyup', e.target.value)
+  const filterInput = () => dispatch(setTagsFilter(inputText))
+  const debouncedFilterInput = debounce(filterInput, 300);
 
-    dispatch(setTagsFilter(e.target.value));
+
+  const onTextChange = (e) => {
+    debouncedFilterInput()
   };
+
 
   const onSortChange = ({value}) => {
     value.sortBy === 'comments' ?
@@ -34,26 +36,16 @@ export const FiltersBar = () => {
 
   return (
     <div className="filters-bar">
-      <DebounceInput
-        type="text"
-        placeholder="search by tags"
-        className="filters-bar__item"
-        value={inputText}
-        debounceTimeout={300} //не работает на onKeyUp, только onChange
-        // onChange={onTextChange}
-        onChange={(e) => {setInputText(e.target.value)}}
-        onKeyUp={onTextChange}
-      />
-
       <input
-        type="text"
+         type="text"
         placeholder="search by tags"
         className="filters-bar__item"
         value={inputText}
-        debounceTimeout={300}
-        onChange={onTextChange}
-        // onChange={(e) => {setInputText(e.target.value)}}
-        // onKeyUp={onTextChange}
+        // onChange={onTextChange}
+        onChange={(e) => {
+          setInputText(e.target.value)
+        }}
+        onKeyUp={onTextChange}
       />
       <Select className="filters-bar__item"
               value={sortBy}
